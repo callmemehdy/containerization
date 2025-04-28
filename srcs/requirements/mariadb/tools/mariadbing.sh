d1998e53d484f82aaac1b1477c11de3556b6413a
mehdi el akary <rock.mido15@gmail.com>
2025-04-28 11:55:02 +0000

    thee beg of inception era

---
+++ srcs/requirements/mariadb/tools/mariadbing.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+service mariadb start
+
+sleep 3
+
+mariadb -e "create database if not exists $MARIADB_DB ;"
+mariadb -e "create user if not exists $MARIADB_USER@'%' identified by '$MARIADB_PASSWORD';"
+mariadb -e "grant all privileges on * . * to $MARIADB_USER@'%' identified by '$MARIADB_PASSWORD';"
+mariadb -e "flush privileges;"
+
+
+mariadb-admin -u root -p$MARIADB_PASSWORD shutdown
+
+
+mariadbd-safe --bind-address=0.0.0.0 --port=3306 --datadir='/var/lib/mysql'
---
+++ srcs/requirements/wordpress/tools/wordpressing.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+
+apt install -y sendmail
+
+chmod +x wp-cli.phar && mv wp-cli.phar /usr/local/bin/wp
+
+
+# in case there multiuser group of www-data group
+chown -R www-data:www-data  /var/www/wordpress
+
+chmod -R 755 /var/www/wordpress
+
+cd /var/www/wordpress
+
+wp core download --allow-root
+
+wp core config	--dbhost=mariadb:3306 --dbname="$MARIADB_DB" --dbuser="$MARIADB_USER" \
+				--dbpass="$MARIADB_PASSWORD" --allow-root
+
+wp core install --url="$DOMAIN_NAME" --title="$WEBSITE_TITLE" --admin_user="$ADMIN_USER" \
+				--admin_password="$ADMIN_PASS" --admin_email="$ADMIN_EMAIL" --allow-root #--locale=$LOCAL
+
+
+wp db check --allow-root
+
+/usr/sbin/php-fpm7.4 -F
