aab802a46c3afaee13b2dd8236aea081aaf4e1e9
mehdi el akary <rock.mido15@gmail.com>
2025-05-08 18:54:40 +0000

    the last touches on the goated inception

--- srcs/requirements/mariadb/tools/mariadbing.sh
+++ srcs/requirements/mariadb/tools/mariadbing.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
 
-service mariadb start
+service mariadb start -uroot -p$MARIADB_PASSWORD_ROOT
  
 
-mariadb -e "create database if not exists $MARIADB_DB ;"
+mariadb -e "create database if not exists $MARIADB_DB;"
 mariadb -e "create user if not exists $MARIADB_USER@'%' identified by '$MARIADB_PASSWORD';"
-mariadb -e "grant all privileges on * . * to $MARIADB_USER@'%' identified by '$MARIADB_PASSWORD';"
+mariadb -e "grant all privileges on $MARIADB_DB.* to $MARIADB_USER@'%'"
 mariadb -e "flush privileges;"
+mariadb -e "alter user 'root'@'localhost' identified by '$MARIADB_ROOT_PASS';"
 
 
-mariadb-admin -u root -p$MARIADB_PASSWORD shutdown
 
+
+mariadb-admin -uroot -p$MARIADB_ROOT_PASS shutdown
 
 mariadbd-safe --bind-address=0.0.0.0 --port=3306 --datadir='/var/lib/mysql'
--- srcs/requirements/wordpress/tools/wordpressing.sh
+++ srcs/requirements/wordpress/tools/wordpressing.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# while ! mysqladmin ping -h"mariadb" -u"${MARIADB_USER}" -p"${MARIADB_PASSWORD}" --silent; do
+#     sleep 1
+# done
 
 
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
@@ -17,7 +20,6 @@ cd /var/www/wordpress
 	
 # fi
 
-# sleep 3
 
 wp core download --allow-root
 
@@ -29,29 +31,31 @@ chmod 775 dx-adminer.php
 
 chown www-data:www-data dx-adminer.php
 
-
-#############
-
-wp core config	--dbhost=mariadb:3306 --dbname="$MARIADB_DB" --dbuser="$MARIADB_USER" \
-				--dbpass="$MARIADB_PASSWORD" --allow-root
-
-wp config set WP_REDIS_HOST 'redis' --add --allow-root
-
-wp config set WP_REDIS_PORT 1234 --add --allow-root
-
-
-wp core install --url="$DOMAIN_NAME" --title="$WEBSITE_TITLE" --admin_user="$ADMIN_USER" \
-				--admin_password="$ADMIN_PASS" --admin_email="$ADMIN_EMAIL" --allow-root
-
-wp user create "$WP_USER" "$WP_USER_EMAIL" --role="$WP_USER_ROLE" --user_pass="$WP_USER_PASS" --allow-root
-
-
 chown -R www-data:www-data  /var/www
 
 chmod -R 775 /var/www/wordpress
 
+#############
+sleep 5
+
+wp core  config	--dbhost=mariadb:3306 --dbname=$MARIADB_DB --dbuser=$MARIADB_USER \
+				--dbpass=$MARIADB_PASSWORD --allow-root
+
+wp config set WP_REDIS_HOST 'redis' --add --allow-root
+
+wp config set WP_REDIS_PORT $REDIS_PORT --add --allow-root
+
+
+wp core install --url=$DOMAIN_NAME --title=$WEBSITE_TITLE --admin_user=$ADMIN_USER \
+				--admin_password=$ADMIN_PASS --admin_email=$ADMIN_EMAIL --allow-root
+
+wp user create $WP_USER $WP_USER_EMAIL --role=$WP_USER_ROLE --user_pass=$WP_USER_PASS --allow-root
+
+
+
 wp redis enable --allow-root
 
 wp db check --allow-root
+
 
 /usr/sbin/php-fpm7.4 -F
